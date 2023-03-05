3e95d6f7a6b29f4b716ff825496fe5e877f6c745
ptrv-dev <petrov99119@gmail.com>
2023-03-05 22:01:46 +0200

    Testimonials Section (not responsive)

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,45 @@
-console.log('Hello from JavaScript!');
+const images = Array.from(
+  document.querySelector('.testimonials__image').childNodes
+).filter((node) => node.nodeName === 'PICTURE');
+const names = Array.from(
+  document.querySelector('.testimonials__name').childNodes
+).filter((node) => node.nodeName === 'H3');
+const positions = Array.from(
+  document.querySelector('.testimonials__position').childNodes
+).filter((node) => node.nodeName === 'P');
+const texts = Array.from(
+  document.querySelector('.testimonials__text').childNodes
+).filter((node) => node.nodeName === 'P');
+
+const navigation = {
+  prev: document.querySelector('.testimonials__button_prev'),
+  next: document.querySelector('.testimonials__button_next'),
+};
+
+let currentSlide = 1;
+const slidesCount = images.length;
+
+const handlePrevious = () => {
+  currentSlide <= 1 ? (currentSlide = slidesCount) : currentSlide--;
+  handleSlideChange();
+};
+
+const handleNext = () => {
+  currentSlide >= slidesCount ? (currentSlide = 1) : currentSlide++;
+  handleSlideChange();
+};
+
+const handleSlideChange = () => {
+  [images, names, positions, texts].forEach((nodeArray) =>
+    nodeArray.forEach((node, idx) => {
+      idx === currentSlide - 1
+        ? (node.dataset.active = true)
+        : (node.dataset.active = false);
+    })
+  );
+};
+
+handleSlideChange();
+
+navigation.prev.addEventListener('click', handlePrevious);
+navigation.next.addEventListener('click', handleNext);
